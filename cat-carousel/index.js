7e908e989b65e772f7e4527d39a4a535e0317dd6
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-07-11 11:46:15 -0500

    attempts made on keypress for cat-carasoul

--- cat-carousel/index.js
+++ cat-carousel/index.js
@@ -3,3 +3,10 @@ $('.thumbnail').on('click', e => {
     let imgAlt = $(e.target).attr('alt');
     $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
   });
+
+  $('.thumbnail').on('keypress' , e => {
+      if( e.keycode == 0 || e.keycode == 32 || e.key == ' ' || e.key == 'Spacebar') {
+            let imgSrc = $(e.target).attr('src');
+            let imgAlt = $(e.target).attr('alt');
+         $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
+  }});
